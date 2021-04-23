673c09a7cd9a985a4c84f3dae904917034d4db33
Tianhe Xie <64594614+tiaxie@users.noreply.github.com>
2021-04-23 20:42:44 +0800

    Add files via upload

--- finalproject195/finalproject195/ggcontroller.swift
+++ finalproject195/finalproject195/ggcontroller.swift
@@ -57,17 +57,113 @@ class ggcontroller: UIViewController{
     @IBOutlet weak var c6grade: UITextField!
 
     
-    var cumulative = 0
+    @IBOutlet weak var targetGPA: UITextField!
+    
+    var cumulative: Float = 0
+    var courses = 0
+    var totcourses = 0
     
     @IBAction func calc(_ sender: Any) {
-        print("btn clicked")
-        
-        if let aatext = aa.text, aatext.isEmpty {
-            cumulative += Int(aatext)!
+//        print("btn clicked")
+//        print(aa.text!)
+//        if var aatext = aa.text, aatext.isEmpty {
+//            aatext = aa.text!
+//            cumulative += Int(aatext)!
+//        } else {
+//           print("is empty")
+//        }
+        //print(cumulative)
+        print(aa.text ?? "")
+        if aa.text == ""{
+            print("null")
         } else {
-           // myTextField is Empty
+            print("not nil")
         }
-        print(cumulative)
+        
+        for i in 1...11
+            {
+                let aa = self.view.viewWithTag(i) as! UITextField
+
+                if aa.text == ""{
+                    
+                } else {
+                   var grade: Float = 0
+                    if i == 1{
+                        grade = 4
+                    } else if i == 2{
+                        grade = 3.7
+                    } else if i == 3{
+                        grade = 3.3
+                    } else if i == 4{
+                        grade = 3.0
+                    } else if i == 5{
+                        grade = 2.7
+                    } else if i == 6{
+                        grade = 2.3
+                    } else if i == 7{
+                        grade = 2.0
+                    } else if i == 8{
+                        grade = 1.7
+                    }else if i == 9{
+                        grade = 1.3
+                    } else if i == 10{
+                        grade = 1.0
+                    } else if i == 11{
+                        grade = 0
+                    }
+                    
+                    let num = Int(aa.text!)
+                    cumulative += Float(num!)*grade
+                    courses += num!
+                    totcourses += num!
+                }
+            }
+        for i in 12...17{
+                let aa = self.view.viewWithTag(i) as! UITextField
+
+                if aa.text == ""{
+                    
+                } else {
+                    let bb = self.view.viewWithTag(i+10) as! UITextField
+                    if bb.text == ""{
+                        totcourses+=1
+                    }
+                    else{
+                        print(bb.text!)
+                    let num = Int(bb.text!)
+                        cumulative += Float(num!)
+                        courses += 1
+                        totcourses += 1
+                    }
+                }
+        }
+        
+        if targetGPA.text == ""{
+            
+        } else{
+            let tobeCalc = totcourses - courses
+            let target: Float = Float(targetGPA.text!)!
+            let targetTotal: Float = Float(target * Float(totcourses))
+            let targetScore = (targetTotal - cumulative)/Float(tobeCalc)
+            if targetScore > 4.0{
+                let aa = self.view.viewWithTag(30) as! UITextField
+                aa.text = "Please adjust your goal"
+            } else{
+                for i in 12...17{
+                    let aa = self.view.viewWithTag(i) as! UITextField
+
+                    if aa.text == ""{
+                        
+                    } else{
+                        let bb = self.view.viewWithTag(i+10) as! UITextField
+                        if bb.text == ""{
+                            bb.text = String(targetScore)
+                        }
+                    }
+                }
+            }
+        }
+        
     }
     
 }
