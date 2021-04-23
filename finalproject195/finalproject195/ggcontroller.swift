23845b0ad10b8062542c4ae134fe651a6eb4e311
Tianhe Xie <64594614+tiaxie@users.noreply.github.com>
2021-04-23 14:43:37 +0800

    Add files via upload

--- finalproject195/finalproject195/ggcontroller.swift
+++ finalproject195/finalproject195/ggcontroller.swift
@@ -8,4 +8,66 @@ import UIKit
 
 class ggcontroller: UIViewController{
     
+    @IBOutlet weak var aa: UITextField!
+    
+    
+    @IBOutlet weak var am: UITextField!
+    
+    
+    @IBOutlet weak var bp: UITextField!
+    
+    
+    @IBOutlet weak var b: UITextField!
+    
+    
+    @IBOutlet weak var bm: UITextField!
+    
+    
+    @IBOutlet weak var cp: UITextField!
+    
+    
+    @IBOutlet weak var c: UITextField!
+    
+    
+    @IBOutlet weak var cm: UITextField!
+    
+    
+    @IBOutlet weak var dp: UITextField!
+    
+    
+    @IBOutlet weak var d: UITextField!
+    
+    
+    @IBOutlet weak var f: UITextField!
+    
+    
+   
+    @IBOutlet weak var c1grade: UITextField!
+    
+    @IBOutlet weak var c2grade: UITextField!
+    
+    @IBOutlet weak var c3grade: UITextField!
+    
+    
+    @IBOutlet weak var c4grade: UITextField!
+
+    @IBOutlet weak var c5grade: UITextField!
+    
+    
+    @IBOutlet weak var c6grade: UITextField!
+
+    
+    var cumulative = 0
+    
+    @IBAction func calc(_ sender: Any) {
+        print("btn clicked")
+        
+        if let aatext = aa.text, aatext.isEmpty {
+            cumulative += Int(aatext)!
+        } else {
+           // myTextField is Empty
+        }
+        print(cumulative)
+    }
+    
 }
